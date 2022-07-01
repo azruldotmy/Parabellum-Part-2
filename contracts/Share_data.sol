dca446f847e47a68b49bec09ce0971d36c1147fe
zulfakaraziz <85026509+zulfakaraziz@users.noreply.github.com>
2022-07-01 23:06:04 +0800

    Update Share_data.sol

--- contracts/Share_data.sol
+++ contracts/Share_data.sol
@@ -1,23 +1,54 @@
 // SPDX-License-Identifier: GPL-3.0
-
 pragma solidity >=0.7.0 <0.9.0;
 
+/**
+ * @title Share Data
+ * @dev Share patient's data
+ * @custom:dev-run-script ./scripts/deploy_with_ethers.ts
+ */
+import {Insert_Data} from "./Insert_Data.sol";
 
-
-contract Share_data {
+contract Share_Data {
 
     address private patient;
+    uint256 public no_of_patient = 0;
+    uint256 public no_of_doctor = 0;
 
-    constructor() public{
-     patient = msg.sender;
+    struct Data{
+        uint id;
+        string name;
+        uint phoneNo;
+        string location;
+        string problem; 
+        uint age;
+        string doc;
+        bool vacant;
+        uint payment;
+        string date;
+        address payable patient;
+    }
+
+    constructor() {
+        patient = msg.sender;
     }
    
-    function getData() public view returns(address) {        //Returns address and balance of the patient
+    function cancerCategory() public view returns(address) {        //Returns address and Cancer Category
         return patient;
     }
 
-     function getBalance() public view returns(uint256){
-        return patient.balance;
+    function patientId() public view returns(address){              //Returns address and Patient ID
+        return patient;
+    }
+
+    function dataRights() public view returns(address) {            //Returns address and Data Rights
+        return patient;
+    }
+
+    function dateFrom() public view returns(address){               //Returns address and Date Start
+        return patient;
+    }
+
+    function dateTo() public view returns(address){                 //Returns address and Date End
+        return patient;
     }
 }
-
